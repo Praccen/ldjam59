394fd0a9b323dcc0a0a5404e084937e27cb8b230
Anton Christoffersson <anton_christoffersson@hotmail.com>
2026-04-19 14:40:20 +0200

    Reduced shadow map resolution

--- src/Engine/Rendering/Renderer/RenderPasses/ShadowPasses/DirectionalShadowRenderPass.ts
+++ src/Engine/Rendering/Renderer/RenderPasses/ShadowPasses/DirectionalShadowRenderPass.ts
@@ -32,7 +32,7 @@ export default class DirectionalShadowRenderPass {
     this.directionalShadowSkeletalAnimationShaderProgram =
       directionalShadowSkeletalAnimationShaderProgram;
     // ---- Shadow mapping ----
-    this.shadowResolution = 6400;
+    this.shadowResolution = 640;
     this.shadowBuffer = new Framebuffer(
       this.gl,
       this.shadowResolution,
